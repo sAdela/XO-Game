16323e97a48a8c0dd6a5e9ba1bb6bbafd91de7eb
Adela Selimović <adelaadria1@outlook.com>
2019-06-30 17:32:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ var clicked = [false, false, false, false, false, false, false, false, false];
 var clickedComp = [false, false, false, false, false, false, false, false, false];
 
 
+/*moram popraviti lastMove, komp zadnji klicke, znaci treba biti jedno false ja mislim*/
 function lastMove(){
     for (var i=0; i<9; i++){
         if (!clicked[i])
@@ -59,39 +60,39 @@ function computerPlay(el){
             else if (clicked[4] == true && clicked[8] == true && clickedComp[4] == false && clickedComp[8] == false)
                 dialog("You won!");
         if(!lastMove()){
-            if (clicked[1] == false && clicked[2] == true){
+            if (clicked[1] == false && clicked[2] == true && clickedComp[2] == false){
                 var id = String.fromCharCode(98);
                 $("#" + id).html("<img src=pictures/oks.png>");
                 clicked[1] = true;
                 clickedComp[1] = true;
             }
-            else if (clicked[1] == true && clicked[2] == false){
+            else if (clicked[1] == true && clicked[2] == false && clickedComp[1] == false){
                 var id = String.fromCharCode(99);
                 $("#" + id).html("<img src=pictures/oks.png>");
                 clicked[2] = true;
                 clickedComp[2] = true;
             }
             
-            else if (clicked[3] == true && clicked[6] == false){
+            else if (clicked[3] == true && clicked[6] == false && clickedComp[3] == false){
                 var id = String.fromCharCode(103);
                 $("#" + id).html("<img src=pictures/oks.png>");
                 clicked[6] = true;
                 clickedComp[6] = true;
             }
-            else if (clicked[3] == false && clicked[6] == true){
+            else if (clicked[3] == false && clicked[6] == true && clickedComp[6] == false){
                 var id = String.fromCharCode(100);
                 $("#" + id).html("<img src=pictures/oks.png>");
                 clicked[3] = true;
                 clickedComp[3] = true;
             }
             
-            else if (clicked[4] == true && clicked[8] == false){
+            else if (clicked[4] == true && clicked[8] == false && clickedComp[4] == false){
                 var id = String.fromCharCode(105);
                 $("#" + id).html("<img src=pictures/oks.png>");
                 clicked[8] = true;
                 clickedComp[8] = true;
             }
-            else if (clicked[4] == false && clicked[8] == true){
+            else if (clicked[4] == false && clicked[8] == true && clickedComp[8] == false){
                 var id = String.fromCharCode(101);
                 $("#" + id).html("<img src=pictures/oks.png>");
                 clicked[4] = true;
@@ -117,26 +118,26 @@ function computerPlay(el){
             else if (clicked[4] == true && clicked[7] == true && clickedComp[4] == false && clickedComp[7] == false)
                 dialog("You won!");
             if(!lastMove()){
-             if(clicked[0] == false && clicked[2] == true){
+             if(clicked[0] == false && clicked[2] == true && clickedComp[2] == false){
                 var id = String.fromCharCode(97);
                 $("#" + id).html("<img src=pictures/oks.png>");
                 clicked[0] = true;
                 clickedComp[0] = true;
             }
-            else if (clicked[0] == true && clicked[2] == false){
+            else if (clicked[0] == true && clicked[2] == false && clickedComp[0] == false){
                 var id = String.fromCharCode(99);
                 $("#" + id).html("<img src=pictures/oks.png>");
                 clicked[2] = true;
                 clickedComp[2] = true;
             }
             
-            else if (clicked[4] == true && clicked[7] == false){
+            else if (clicked[4] == true && clicked[7] == false && clickedComp[4] == false){
                 var id = String.fromCharCode(104);
                 $("#" + id).html("<img src=pictures/oks.png>");
                 clicked[7] = true;
                 clickedComp[7] = true;
             }
-            else if (clicked[4] == false && clicked[7] == true){
+            else if (clicked[4] == false && clicked[7] == true && clickedComp[7] == false){
                 var id = String.fromCharCode(101);
                 $("#" + id).html("<img src=pictures/oks.png>");
                 clicked[4] = true;
@@ -164,39 +165,39 @@ function computerPlay(el){
             else if (clicked[4] == true && clicked[6] == true && clickedComp[4] == false && clickedComp[6] == false)
                 dialog("You won!");
             if(!lastMove()){
-             if(clicked[0] == false && clicked[1] == true){
+             if(clicked[0] == false && clicked[1] == true && clickedComp[1] == false){
                 var id = String.fromCharCode(97);
                 $("#" + id).html("<img src=pictures/oks.png>");
                 clicked[0] = true;
                 clickedComp[0] = true;
             }
-            else if (clicked[0] == true && clicked[1] == false){
+            else if (clicked[0] == true && clicked[1] == false && clickedComp[0] == false){
                 var id = String.fromCharCode(98);
                 $("#" + id).html("<img src=pictures/oks.png>");
                 clicked[1] = true;
                 clickedComp[1] = true;
             }
             
-            else if (clicked[5] == true && clicked[8] == false){
+            else if (clicked[5] == true && clicked[8] == false && clickedComp[5] == false){
                 var id = String.fromCharCode(105);
                 $("#" + id).html("<img src=pictures/oks.png>");
                 clicked[8] = true;
                 clickedComp[8] = true;
             }
-            else if (clicked[5] == false && clicked[8] == true){
+            else if (clicked[5] == false && clicked[8] == true && clickedComp[8] == false){
                 var id = String.fromCharCode(102);
                 $("#" + id).html("<img src=pictures/oks.png>");
                 clicked[5] = true;
                 clickedComp[5] = true;
             }
             
-            else if (clicked[4] == true && clicked[6] == false){
+            else if (clicked[4] == true && clicked[6] == false && clickedComp[4] == false){
                 var id = String.fromCharCode(103);
                 $("#" + id).html("<img src=pictures/oks.png>");
                 clicked[6] = true;
                 clickedComp[6] = true;
             }
-            else if (clicked[4] == false && clicked[6] == true){
+            else if (clicked[4] == false && clicked[6] == true && clickedComp[6] == false){
                 var id = String.fromCharCode(101);
                 $("#" + id).html("<img src=pictures/oks.png>");
                 clicked[4] = true;
@@ -221,26 +222,26 @@ function computerPlay(el){
             else if (clicked[4] == true && clicked[5] == true && clickedComp[4] == false && clickedComp[5] == false)
                 dialog("You won!");
             if(!lastMove()){
-             if(clicked[0] == false && clicked[6] == true){
+             if(clicked[0] == false && clicked[6] == true && clickedComp[6] == false){
                 var id = String.fromCharCode(97);
                 $("#" + id).html("<img src=pictures/oks.png>");
                 clicked[0] = true;
                 clickedComp[0] = true;
             }
-            else if (clicked[0] == true && clicked[6] == false){
+            else if (clicked[0] == true && clicked[6] == false && clickedComp[0] == false){
                 var id = String.fromCharCode(103);
                 $("#" + id).html("<img src=pictures/oks.png>");
                 clicked[6] = true;
                 clickedComp[6] = true;
             }
            
-            else if (clicked[4] == true && clicked[5] == false){
+            else if (clicked[4] == true && clicked[5] == false && clickedComp[4] == false){
                 var id = String.fromCharCode(102);
                 $("#" + id).html("<img src=pictures/oks.png>");
                 clicked[5] = true;
                 clickedComp[5] = true;
             }
-            else if (clicked[4] == false && clicked[5] == true){
+            else if (clicked[4] == false && clicked[5] == true && clickedComp[5] == false){
                 var id = String.fromCharCode(101);
                 $("#" + id).html("<img src=pictures/oks.png>");
                 clicked[4] = true;
@@ -270,52 +271,52 @@ function computerPlay(el){
             else if (clicked[3] == true && clicked[5] == true && clickedComp[3] == false && clickedComp[5] == false)
                 dialog("You won!");
             if(!lastMove()){
-             if(clicked[0] == false && clicked[8] == true){
+             if(clicked[0] == false && clicked[8] == true && clickedComp[8] == false){
                 var id = String.fromCharCode(97);
                 $("#" + id).html("<img src=pictures/oks.png>");
                 clicked[0] = true;
                 clickedComp[0] = true;
             }
-            else if (clicked[0] == true && clicked[8] == false){
+            else if (clicked[0] == true && clicked[8] == false && clickedComp[0] == false){
                 var id = String.fromCharCode(105);
                 $("#" + id).html("<img src=pictures/oks.png>");
                 clicked[8] = true;
                 clickedComp[8] = true;
             }
             
-            else if (clicked[1] == true && clicked[7] == false){
+            else if (clicked[1] == true && clicked[7] == false && clickedComp[1] == false){
                 var id = String.fromCharCode(104);
                 $("#" + id).html("<img src=pictures/oks.png>");
                 clicked[7] = true;
                 clickedComp[7] = true;
             }
-            else if (clicked[1] == false && clicked[7] == true){
+            else if (clicked[1] == false && clicked[7] == true && clickedComp[7] == false){
                 var id = String.fromCharCode(98);
                 $("#" + id).html("<img src=pictures/oks.png>");
                 clicked[1] = true;
                 clickedComp[1] = true;
             }
            
-            else if (clicked[2] == true && clicked[6] == false){
+            else if (clicked[2] == true && clicked[6] == false && clickedComp[2] == false){
                 var id = String.fromCharCode(103);
                 $("#" + id).html("<img src=pictures/oks.png>");
                 clicked[6] = true;
                 clickedComp[6] = true;
             }
-            else if (clicked[2] == false && clicked[6] == true){
+            else if (clicked[2] == false && clicked[6] == true && clickedComp[6] == false){
                 var id = String.fromCharCode(99);
                 $("#" + id).html("<img src=pictures/oks.png>");
                 clicked[2] = true;
                 clickedComp[2] = true;
             }
             
-            else if (clicked[3] == true && clicked[5] == false){
+            else if (clicked[3] == true && clicked[5] == false && clickedComp[3] == false){
                 var id = String.fromCharCode(102);
                 $("#" + id).html("<img src=pictures/oks.png>");
                 clicked[5] = true;
                 clickedComp[5] = true;
             }
-            else if (clicked[3] == false && clicked[5] == true){
+            else if (clicked[3] == false && clicked[5] == true && clickedComp[5] == false){
                 var id = String.fromCharCode(100);
                 $("#" + id).html("<img src=pictures/oks.png>");
                 clicked[3] = true;
@@ -341,26 +342,26 @@ function computerPlay(el){
             else if (clicked[3] == true && clicked[4] == true && clickedComp[3] == false && clickedComp[4] == false)
                 dialog("You won!");
             if(!lastMove()){
-             if(clicked[2] == false && clicked[8] == true){
+             if(clicked[2] == false && clicked[8] == true && clickedComp[8] == false){
                 var id = String.fromCharCode(99);
                 $("#" + id).html("<img src=pictures/oks.png>");
                 clicked[2] = true;
                 clickedComp[2] = true;
             }
-            else if (clicked[2] == true && clicked[8] == false){
+            else if (clicked[2] == true && clicked[8] == false && clickedComp[2] == false){
                 var id = String.fromCharCode(105);
                 $("#" + id).html("<img src=pictures/oks.png>");
                 clicked[8] = true;
                 clickedComp[8] = true;
             }
             
-            else if (clicked[3] == true && clicked[4] == false){
+            else if (clicked[3] == true && clicked[4] == false && clickedComp[3] == false){
                 var id = String.fromCharCode(101);
                 $("#" + id).html("<img src=pictures/oks.png>");
                 clicked[4] = true;
                 clickedComp[4] = true;
             }
-            else if (clicked[3] == false && clicked[4] == true){
+            else if (clicked[3] == false && clicked[4] == true && clickedComp[4] == false){
                 var id = String.fromCharCode(100);
                 $("#" + id).html("<img src=pictures/oks.png>");
                 clicked[3] = true;
@@ -388,39 +389,39 @@ function computerPlay(el){
             if (clicked[7] == true && clicked[8] == true && clickedComp[7] == false && clickedComp[8] == false)
                 dialog("You won!");
             if(!lastMove()){
-             if(clicked[0] == false && clicked[3] == true){
+             if(clicked[0] == false && clicked[3] == true && clickedComp[3] == false){
                 var id = String.fromCharCode(97);
                 $("#" + id).html("<img src=pictures/oks.png>");
                 clicked[0] = true;
                 clickedComp[0] = true;
             }
-            else if (clicked[0] == true && clicked[3] == false){
+            else if (clicked[0] == true && clicked[3] == false && clickedComp[0] == false){
                 var id = String.fromCharCode(100);
                 $("#" + id).html("<img src=pictures/oks.png>");
                 clicked[3] = true;
                 clickedComp[3] = true;
             }
             
-            else if (clicked[2] == true && clicked[4] == false){
+            else if (clicked[2] == true && clicked[4] == false && clickedComp[2] == false){
                 var id = String.fromCharCode(101);
                 $("#" + id).html("<img src=pictures/oks.png>");
                 clicked[4] = true;
                 clickedComp[4] = true;
             }
-            else if (clicked[2] == false && clicked[4] == true){
+            else if (clicked[2] == false && clicked[4] == true && clickedComp[4] == false){
                 var id = String.fromCharCode(99);
                 $("#" + id).html("<img src=pictures/oks.png>");
                 clicked[2] = true;
                 clickedComp[2] = true;
             }
             
-            else if (clicked[7] == true && clicked[8] == false){
+            else if (clicked[7] == true && clicked[8] == false && clickedComp[7] == false){
                 var id = String.fromCharCode(105);
                 $("#" + id).html("<img src=pictures/oks.png>");
                 clicked[8] = true;
                 clickedComp[8] = true;
             }
-            else if (clicked[7] == false && clicked[8] == true){
+            else if (clicked[7] == false && clicked[8] == true && clickedComp[8] == false){
                 var id = String.fromCharCode(104);
                 $("#" + id).html("<img src=pictures/oks.png>");
                 clicked[7] = true;
@@ -446,26 +447,26 @@ function computerPlay(el){
             else if (clicked[6] == true && clicked[8] == true && clickedComp[6] == false && clickedComp[8] == false)
                 dialog("You won!");
             if(!lastMove()){
-             if(clicked[1] == false && clicked[4] == true){
+             if(clicked[1] == false && clicked[4] == true && clickedComp[4] == false){
                 var id = String.fromCharCode(98);
                 $("#" + id).html("<img src=pictures/oks.png>");
                 clicked[1] = true;
                 clickedComp[1] = true;
             }
-            else if (clicked[1] == true && clicked[4] == false){
+            else if (clicked[1] == true && clicked[4] == false && clickedComp[1] == false){
                 var id = String.fromCharCode(101);
                 $("#" + id).html("<img src=pictures/oks.png>");
                 clicked[4] = true;
                 clickedComp[4] = true;
             }
             
-            else if (clicked[6] == true && clicked[8] == false){
+            else if (clicked[6] == true && clicked[8] == false && clickedComp[6] == false){
                 var id = String.fromCharCode(105);
                 $("#" + id).html("<img src=pictures/oks.png>");
                 clicked[8] = true;
                 clickedComp[8] = true;
             }
-            else if (clicked[6] == false && clicked[8] == true){
+            else if (clicked[6] == false && clicked[8] == true && clickedComp[8] == false){
                 var id = String.fromCharCode(103);
                 $("#" + id).html("<img src=pictures/oks.png>");
                 clicked[6] = true;
@@ -493,39 +494,39 @@ function computerPlay(el){
             else if (clicked[6] == true && clicked[7] == true && clickedComp[6] == false && clickedComp[7] == false)
                 dialog("You won!"); 
             if(!lastMove()){
-             if(clicked[0] == false && clicked[4] == true){
+             if(clicked[0] == false && clicked[4] == true && clickedComp[4] == false){
                 var id = String.fromCharCode(97);
                 $("#" + id).html("<img src=pictures/oks.png>");
                 clicked[0] = true;
                 clickedComp[0] = true;
             }
-            else if (clicked[0] == true && clicked[4] == false){
+            else if (clicked[0] == true && clicked[4] == false && clickedComp[0] == false){
                 var id = String.fromCharCode(101);
                 $("#" + id).html("<img src=pictures/oks.png>");
                 clicked[4] = true;
                 clickedComp[4] = true;
             }
             
-            else if (clicked[2] == true && clicked[5] == false){
+            else if (clicked[2] == true && clicked[5] == false && clickedComp[2] == false){
                 var id = String.fromCharCode(102);
                 $("#" + id).html("<img src=pictures/oks.png>");
                 clicked[5] = true;
                 clickedComp[5] = true;
             }
-            else if (clicked[2] == false && clicked[5] == true){
+            else if (clicked[2] == false && clicked[5] == true && clickedComp[5] == false){
                 var id = String.fromCharCode(99);
                 $("#" + id).html("<img src=pictures/oks.png>");
                 clicked[2] = true;
                 clickedComp[2] = true;
             }
             
-            else if (clicked[6] == true && clicked[7] == false){
+            else if (clicked[6] == true && clicked[7] == false && clickedComp[6] == false){
                 var id = String.fromCharCode(104);
                 $("#" + id).html("<img src=pictures/oks.png>");
                 clicked[7] = true;
                 clickedComp[7] = true;
             }
-            else if (clicked[6] == false && clicked[7] == true){
+            else if (clicked[6] == false && clicked[7] == true && clickedComp[7] == false){
                 var id = String.fromCharCode(103);
                 $("#" + id).html("<img src=pictures/oks.png>");
                 clicked[6] = true;
