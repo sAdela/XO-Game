097c5a0b0d1c2ab245956ec4bab80323326bbe3b
Adela Selimović <adelaadria1@outlook.com>
2019-06-30 18:15:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -52,12 +52,18 @@ function computerPlay(el){
         var played = el.id.charCodeAt() - 97;
         if(played == 0){
             
-            if(clicked[1] == true && clicked[2] == true && clickedComp[1] == false && clickedComp[2] == false)
-                dialog("You won");
-            else if (clicked[3] == true && clicked[6] == true && clickedComp[3] == false && clickedComp[6] == false)
-                dialog("You won");
-            else if (clicked[4] == true && clicked[8] == true && clickedComp[4] == false && clickedComp[8] == false)
+            if(clicked[1] == true && clicked[2] == true && clickedComp[1] == false && clickedComp[2] == false){
                 dialog("You won!");
+                return;
+            }
+            else if (clicked[3] == true && clicked[6] == true && clickedComp[3] == false && clickedComp[6] == false){
+                dialog("You won!");
+                return;
+            }
+            else if (clicked[4] == true && clicked[8] == true && clickedComp[4] == false && clickedComp[8] == false){
+                dialog("You won!");
+                return;
+            }
         if(!lastMove()){
             if (clicked[1] == false && clicked[2] == true && clickedComp[2] == false){
                 var id = String.fromCharCode(98);
@@ -112,10 +118,14 @@ function computerPlay(el){
             }  
         }
         else if(played == 1){
-            if(clicked[0] == true && clicked[2] == true && clickedComp[0] == false && clickedComp[2] == false)
+            if(clicked[0] == true && clicked[2] == true && clickedComp[0] == false && clickedComp[2] == false){
                 dialog("You won!");
-            else if (clicked[4] == true && clicked[7] == true && clickedComp[4] == false && clickedComp[7] == false)
+                return;
+            }
+            else if (clicked[4] == true && clicked[7] == true && clickedComp[4] == false && clickedComp[7] == false){
                 dialog("You won!");
+                return;
+            }
             if(!lastMove()){
              if(clicked[0] == false && clicked[2] == true && clickedComp[2] == false){
                 var id = String.fromCharCode(97);
@@ -157,12 +167,18 @@ function computerPlay(el){
             }
         }
         else if(played == 2){
-            if(clicked[0] == true && clicked[1] == true && clickedComp[0] == false && clickedComp[1] == false)
+            if(clicked[0] == true && clicked[1] == true && clickedComp[0] == false && clickedComp[1] == false){
                 dialog("You won!");
-            else if (clicked[5] == true && clicked[8] == true && clickedComp[5] == false && clickedComp[8] == false)
+                return;
+            }
+            else if (clicked[5] == true && clicked[8] == true && clickedComp[5] == false && clickedComp[8] == false){
                 dialog("You won!");
-            else if (clicked[4] == true && clicked[6] == true && clickedComp[4] == false && clickedComp[6] == false)
+                return;
+            }
+            else if (clicked[4] == true && clicked[6] == true && clickedComp[4] == false && clickedComp[6] == false){
                 dialog("You won!");
+                return;
+            }
             if(!lastMove()){
              if(clicked[0] == false && clicked[1] == true && clickedComp[1] == false){
                 var id = String.fromCharCode(97);
@@ -216,10 +232,14 @@ function computerPlay(el){
             }
         }
         else if (played == 3){
-            if(clicked[0] == true && clicked[6] == true && clickedComp[0] == false && clickedComp[6] == false)
+            if(clicked[0] == true && clicked[6] == true && clickedComp[0] == false && clickedComp[6] == false){
                 dialog("You won!");
-            else if (clicked[4] == true && clicked[5] == true && clickedComp[4] == false && clickedComp[5] == false)
+                return;
+            }
+            else if (clicked[4] == true && clicked[5] == true && clickedComp[4] == false && clickedComp[5] == false){
                 dialog("You won!");
+                return;
+            }
             if(!lastMove()){
              if(clicked[0] == false && clicked[6] == true && clickedComp[6] == false){
                 var id = String.fromCharCode(97);
@@ -261,14 +281,22 @@ function computerPlay(el){
             }
         }
         else if (played == 4){
-            if(clicked[0] == true && clicked[8] == true && clickedComp[0] == false && clickedComp[8] == false)
+            if(clicked[0] == true && clicked[8] == true && clickedComp[0] == false && clickedComp[8] == false){
                 dialog("You won!");
-            else if (clicked[1] == true && clicked[7] == true && clickedComp[1] == false && clickedComp[7] == false)
+                return;
+            }
+            else if (clicked[1] == true && clicked[7] == true && clickedComp[1] == false && clickedComp[7] == false){
                 dialog("You won!");
-            else if (clicked[2] == true && clicked[6] == true && clickedComp[2] == false && clickedComp[6] == false)
+                return;
+            }
+            else if (clicked[2] == true && clicked[6] == true && clickedComp[2] == false && clickedComp[6] == false){
                 dialog("You won!");
-            else if (clicked[3] == true && clicked[5] == true && clickedComp[3] == false && clickedComp[5] == false)
+                return;
+            }
+            else if (clicked[3] == true && clicked[5] == true && clickedComp[3] == false && clickedComp[5] == false){
                 dialog("You won!");
+                return;
+            }
             if(!lastMove()){
              if(clicked[0] == false && clicked[8] == true && clickedComp[8] == false){
                 var id = String.fromCharCode(97);
@@ -336,10 +364,14 @@ function computerPlay(el){
             }
         }
         else if (played == 5){
-            if(clicked[2] == true && clicked[8] == true && clickedComp[2] == false && clickedComp[8] == false)
+            if(clicked[2] == true && clicked[8] == true && clickedComp[2] == false && clickedComp[8] == false){
                 dialog("You won!");
-            else if (clicked[3] == true && clicked[4] == true && clickedComp[3] == false && clickedComp[4] == false)
+                return;
+            }
+            else if (clicked[3] == true && clicked[4] == true && clickedComp[3] == false && clickedComp[4] == false){
                 dialog("You won!");
+                return;
+            }
             if(!lastMove()){
              if(clicked[2] == false && clicked[8] == true && clickedComp[8] == false){
                 var id = String.fromCharCode(99);
@@ -381,12 +413,18 @@ function computerPlay(el){
             }
         }
         else if (played == 6){
-            if(clicked[0] == true && clicked[3] == true && clickedComp[0] == false && clickedComp[3] == false)
+            if(clicked[0] == true && clicked[3] == true && clickedComp[0] == false && clickedComp[3] == false){
                 dialog("You won!");
-            if (clicked[2] == true && clicked[4] == true && clickedComp[2] == false && clickedComp[4] == false)
+                return;
+            }
+            if (clicked[2] == true && clicked[4] == true && clickedComp[2] == false && clickedComp[4] == false){
                 dialog("You won!");
-            if (clicked[7] == true && clicked[8] == true && clickedComp[7] == false && clickedComp[8] == false)
+                return;
+            }
+            if (clicked[7] == true && clicked[8] == true && clickedComp[7] == false && clickedComp[8] == false){
                 dialog("You won!");
+                return;
+            }
             if(!lastMove()){
              if(clicked[0] == false && clicked[3] == true && clickedComp[3] == false){
                 var id = String.fromCharCode(97);
@@ -441,10 +479,14 @@ function computerPlay(el){
             }
         }
         else if(played == 7){
-            if(clicked[1] == true && clicked[4] == true && clickedComp[1] == false && clickedComp[4] == false)
+            if(clicked[1] == true && clicked[4] == true && clickedComp[1] == false && clickedComp[4] == false){
                 dialog("You won!");
-            else if (clicked[6] == true && clicked[8] == true && clickedComp[6] == false && clickedComp[8] == false)
+                return;
+            }
+            else if (clicked[6] == true && clicked[8] == true && clickedComp[6] == false && clickedComp[8] == false){
                 dialog("You won!");
+                return;
+            }
             if(!lastMove()){
              if(clicked[1] == false && clicked[4] == true && clickedComp[4] == false){
                 var id = String.fromCharCode(98);
@@ -486,12 +528,18 @@ function computerPlay(el){
             }
         }
         else if(played == 8){
-            if(clicked[0] == true && clicked[4] == true && clickedComp[0] == false && clickedComp[4] == false)
+            if(clicked[0] == true && clicked[4] == true && clickedComp[0] == false && clickedComp[4] == false){
                 dialog("You won!");
-            else if (clicked[2] == true && clicked[5] == true && clickedComp[2] == false && clickedComp[5] == false)
+                return;
+            }
+            else if (clicked[2] == true && clicked[5] == true && clickedComp[2] == false && clickedComp[5] == false){
                 dialog("You won!");
-            else if (clicked[6] == true && clicked[7] == true && clickedComp[6] == false && clickedComp[7] == false)
-                dialog("You won!"); 
+                return;
+            }
+            else if (clicked[6] == true && clicked[7] == true && clickedComp[6] == false && clickedComp[7] == false){
+                dialog("You won!");
+                return;
+            }
             if(!lastMove()){
              if(clicked[0] == false && clicked[4] == true && clickedComp[4] == false){
                 var id = String.fromCharCode(97);
